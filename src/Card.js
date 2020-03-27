ca6ed03f8deba66600b069a6de26b3db9ef1a137
unknown <baigbma@C001806268.mw.na.cat.com>
2020-03-27 17:03:32 -0500

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Card = () => {
+    return(
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div>
+            <img alt='robots' src ='https://robohash.org/test?200x200' />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p> 
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default Card;
