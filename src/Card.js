94465162a80a5a3d4004e7edff46f321c8ecea60
unknown <baigbma@C001806268.mw.na.cat.com>
2020-04-06 20:10:53 -0500

    initial commit

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const Card = () => {
+const Card = (props) => {
     return(
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
         <div>
-            <img alt='robots' src ='https://robohash.org/test?200x200' />
+            <img alt='robots' src ={`https://robohash.org/${props.id}?200x200`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p> 
